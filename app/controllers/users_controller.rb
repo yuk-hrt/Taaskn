267362bf50f5318602a4970e5822d7ccfbcea39f
yuk-hrt <62339023+yuk-hrt@users.noreply.github.com>
2020-08-12 14:34:18 +0900

    task順番変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @lists = @user.lists
+    @lists = @user.lists.includes(:cards).order('created_at DESC')
   end
 
 end
